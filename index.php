42ecfa78e792e1fbf92e79a8d655c952eb9c6c56
LuCaSkooo1 <93703596+LuCaSkooo1@users.noreply.github.com>
2023-05-24 13:07:09 +0200

    php-a2 start

--- index.php
+++ index.php
@@ -5,17 +5,28 @@
 </head>
 <body>
     <h1>Logs:</h1>
-    <p></p>
+    <form action="index.php" method="get">
+        Name: <input type="text" name="name"><br>
+        <input type="submit">
+    </form>
     <?php
 
         $logDate = date('H:i:s');
         $logFile = "logs.txt";
+        $studentFile = "students.json";
         $currentHour = (int) date('H');
 
+        
 
-        function addLog($isLate, $logDate, $logFile, $currentHour)
+
+        function addLog($logDate, $logFile, $currentHour, $studentFile)
         {
             $data = null;
+            $studentData = null;
+
+            if (isset($_GET['name'])) {
+                $name = $_GET['name'];
+            } 
 
             if (($currentHour) >= 8) {
                 $isLate = true;
@@ -23,11 +34,27 @@
 
 
             if ($isLate) {
-                $data = $logDate . " meskanie";
-                $isLate = true;
+                $data = "\n" . $name . " " . $logDate . " meskanie";
+                $studentData = "\n" . $name;
             }else{
-                $data = "\n" . $logDate;
+                $data = "\n" . $name . $logDate;
+                $studentData = "\n" . $name;
             };
+
+            $studentFileData = file_get_contents($studentFile);
+
+                $log = json_decode($studentFileData, true);
+
+                if (array_key_exists($name, $log)) {
+                    $log[$name]++;
+                } else {
+                    $log[$name] = 1;
+                }
+
+                $jsonData = json_encode($log, JSON_PRETTY_PRINT);
+
+                file_put_contents($studentFile, $jsonData);
+
 
             if(($currentHour) >= 20 && ($currentHour) <= 24 ){
                 die("prichod nemoze byt zaznamenany");
@@ -37,21 +64,17 @@
             $handle = fopen($logFile, "a");
             fwrite($handle, $data );
             fclose($handle);
+
+            
         }
 
         function getLogs($logFile)
         {
-
-            $lines = file($logFile);
-            
-            foreach ($lines as $line) {
-                echo $line  . "<br>";
-            }
-
+            return file_get_contents($logFile);
         }
 
-        addLog($isLate, $logDate, $logFile, $currentHour);
-        getLogs($logFile);
+        addLog($logDate, $logFile, $currentHour, $studentFile);
+        echo nl2br(getLogs($logFile));
 
         ?>
     
